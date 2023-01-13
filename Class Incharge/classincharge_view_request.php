0a0ef6975a286fc0a75de44349ec2a2a4efb5f21
SNEHAT13 <94219258+SNEHAT13@users.noreply.github.com>
2023-01-13 22:18:44 +0530

    Add files via upload

---
+++ Class Incharge/classincharge_view_request.php
@@ -0,0 +1,177 @@
+<?php
+session_start();      
+    $servername = "localhost";
+    $username = "root";
+    $password = "Tamil@141101";
+    $dbname = "rec_hostel";  
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$conn) {
+    alert("Connection failed: " . mysqli_connect_error());
+  }
+  if (!isset($_GET['username'])) {
+    header('Location: main.php');
+    exit;
+  }
+  // Extract the IV from the encrypted data
+$iv_length = openssl_cipher_iv_length('aes-128-cbc');
+$iv = substr($_GET['username'], 0, $iv_length);
+$username_encrypted = substr($_GET['username'], $iv_length);
+
+// Decode the encrypted data
+$username_encrypted = urldecode($username_encrypted);
+
+// Decrypt the username using the same secret key and IV
+$key = 'secretkey';
+$username = openssl_decrypt(base64_decode($username_encrypted), 'aes-128-cbc', $key, 0, $iv);
+
+// Select all rows from the leave_forms table
+echo '<h1><center>Leave Request</center></h1>';
+echo '<style>
+        table {
+          table-layout: fixed;
+            }
+          td, th {
+            word-wrap: break-word;
+              }
+            </style>';
+        echo '<table class="table table-striped" border="5">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th style="vertical-align: middle;">Name</th>';
+        echo '<th style="vertical-align: middle;">Register Number</th>';
+        echo '<th style="vertical-align: middle;">Department & Section</th>';
+        echo '<th style="vertical-align: middle;">Reason for Leave</th>';
+        echo '<th style="vertical-align: middle;">Applied Date & Time</th>';
+        echo '<th style="vertical-align: middle;">Leave Start Date & End Date</th>';
+        echo '<th style="vertical-align: middle;">Leave Start Time & End Time</th>';
+        echo '<th style="vertical-align: middle;">Class incharge Status</th>';
+        echo '<th style="vertical-align: middle;">Action</th>';
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+if ($username) {
+  $sql = "SELECT department FROM classincharge WHERE username='$username'";
+  $result = mysqli_query($conn, $sql);
+      if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $department = $row['department'];
+
+         if($department == 'EEE - A') {
+            $sql = "SELECT * FROM student_leave_request WHERE department='EEE - A' AND classincharge_status='Pending'"; 
+            $result = mysqli_query($conn, $sql);
+               if (mysqli_num_rows($result) > 0) {
+                  
+        while ($row = mysqli_fetch_assoc($result)) {
+          echo '<tr>';
+          echo '<td>' . $row['name'] . '</td>';
+          echo '<td>' . $row['register_number'] . '</td>';
+          echo '<td>' . $row['department'] . '</td>';
+          echo '<td>' . $row['reason'] . '</td>';
+          echo '<td>' . $row['apply_date'] .'<br>' . $row['apply_time'] . '</td>';
+          echo '<td>' . $row['start_date'] .'<br>' . $row['end_date'] . '</td>';
+          echo '<td>' . $row['start_time'] .'<br>' . $row['end_time'] . '</td>';
+          echo '<td>' . $row['classincharge_status'] . '</td>';
+          echo '<td>
+             <form action="" method="post">
+              <input type="hidden" name="id" value="' . $row['id'] . '">
+              <button type="submit" name="accept" class="btn btn-success" style="padding: 5px 5px;">Accept</button>
+              <button type="submit" name="reject" class="btn btn-danger" style="padding: 5px 8px;">Reject</button>
+              </form>
+            </td>';
+          echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
+      } 
+      else {
+        echo '<table class="table table-striped" border="1">';
+        echo '<tbody>';
+        echo '<tr>';
+        echo '<td><center>No request found</center></td>';
+        echo '</tr>';
+        echo '</tbody>';
+        echo '</table>';
+      }
+
+    }
+
+    if ($department == 'IT - B') {
+      $sql = "SELECT * FROM student_leave_request WHERE department='IT - B' AND classincharge_status='Pending'"; 
+
+      $result = mysqli_query($conn, $sql);
+      
+        if (mysqli_num_rows($result) > 0) {
+           
+         while ($row = mysqli_fetch_assoc($result)) {
+        echo '<tr>';
+        echo '<td>' . $row['name'] . '</td>';
+        echo '<td>' . $row['register_number'] . '</td>';
+        echo '<td>' . $row['department'] . '</td>';
+        echo '<td>' . $row['reason'] . '</td>';
+        echo '<td>' . $row['apply_date'] .'<br>' . $row['apply_time'] . '</td>';
+        echo '<td>' . $row['start_date'] .'<br>' . $row['end_date'] . '</td>';
+        echo '<td>' . $row['start_time'] .'<br>' . $row['end_time'] . '</td>';
+        echo '<td>' . $row['classincharge_status'] . '</td>';
+        echo '<td>
+      <form action="" method="post">
+       <input type="hidden" name="id" value="' . $row['id'] . '">
+       <button type="submit" name="accept" class="btn btn-success" style="padding: 5px 5px;">Accept</button>
+       <button type="submit" name="reject" class="btn btn-danger" style="padding: 5px 8px;">Reject</button>
+       </form>
+     </td>';
+       echo '</tr>';
+     }
+       echo '</tbody>';
+       echo '</table>';
+   } 
+    else {
+       echo '<table class="table table-striped" border="1">';
+       echo '<tbody>';
+       echo '<tr>';
+       echo '<td><center>No request found</center></td>';
+       echo '</tr>';
+       echo '</tbody>';
+       echo '</table>';
+      }
+    }
+   }
+ }
+
+if (isset($_POST['accept'])) {
+  $id = mysqli_real_escape_string($conn, $_POST['id']);
+  $sql = "UPDATE student_leave_request SET classincharge_status='Accepted' WHERE id='$id'";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_affected_rows($conn) > 0) {
+    header("Location: {$_SERVER['REQUEST_URI']}");
+    exit;
+  }
+  
+} 
+elseif (isset($_POST['reject'])) {
+  $id = mysqli_real_escape_string($conn, $_POST['id']);
+  $sql = "UPDATE student_leave_request SET classincharge_status='Rejected' WHERE id='$id'";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_affected_rows($conn) > 0) {
+    header("Location: {$_SERVER['REQUEST_URI']}");
+    exit;
+  }
+}
+
+mysqli_close($conn);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <title>Class Incharge View Leave Request</title>
+</head>
+<body>
+  
+</body>
+</html>
